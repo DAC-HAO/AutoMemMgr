e87e5fb0865e6f48ee5098ccc122c0e46cc0469d
superhao1995 <804673818@qq.com>
2023-01-12 13:57:13 +0800

    cuda stream test

---
+++ test_cuda_stream.py
@@ -0,0 +1,30 @@
+import time
+import torch
+
+a = torch.rand((1024, 1024), pin_memory=True)
+b = torch.rand((64, 64), device="cuda")
+c = torch.rand((64, 64), device="cuda")
+
+# s1 = torch.cuda.Stream()
+s2 = torch.cuda.Stream()
+
+torch.cuda.synchronize()
+start = time.time()
+
+# with torch.cuda.stream(s1):
+d = b * c
+
+with torch.cuda.stream(s2):
+    a_cuda = torch.empty((1024, 1024), device="cuda")
+    a_cuda.copy_(a, non_blocking=True)
+
+torch.cuda.synchronize()
+print("asyn", time.time() - start)
+
+torch.cuda.synchronize()
+start = time.time()
+e = b * c
+a_cuda = torch.empty((1024, 1024), device="cuda")
+a_cuda.copy_(a, non_blocking=True)
+torch.cuda.synchronize()
+print("syn", time.time() - start)
