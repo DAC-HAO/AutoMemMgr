10ef85d805a2315b5226ef16c920fc292bf30cfc
superhao1995 <804673818@qq.com>
2023-01-12 13:07:06 +0800

    comm test

--- comm_profiler.py
+++ comm_profiler.py
@@ -9,13 +9,13 @@ print("to pin time", time.time() - start_time)
 
 torch.cuda.synchronize()
 start_time = time.time()
-cuda_pin_a = pin_a.cuda()
+cuda_pin_a = pin_a.to("cuda")
 torch.cuda.synchronize()
 print("pin to cuda time", time.time() - start_time)
 
 torch.cuda.synchronize()
 start_time = time.time()
-cuda_a = a.cuda()
+cuda_a = a.to("cuda")
 torch.cuda.synchronize()
 print("cpu to cuda time", time.time() - start_time)
 
