975e560a90eb931c9a9c3e5e69a0836c86fcf9af
superhao1995 <804673818@qq.com>
2023-01-12 14:06:05 +0800

    test

--- test_cuda_stream.py
+++ test_cuda_stream.py
@@ -8,23 +8,23 @@ c = torch.rand((256, 256), device="cuda")
 # comp_stream = torch.cuda.Stream()
 comm_stream = torch.cuda.Stream()
 
-# torch.cuda.synchronize()
-# start = time.time()
-#
-# # with torch.cuda.stream(s1):
-# d = b * c
-#
-# with torch.cuda.stream(comm_stream):
-#     a_cuda = torch.empty((4096, 4096), device="cuda")
-#     a_cuda.copy_(a, non_blocking=True)
-#
-# torch.cuda.synchronize()
-# print("asyn", time.time() - start)
-
 torch.cuda.synchronize()
 start = time.time()
-e = b * c
-a_cuda = torch.empty((4096, 4096), device="cuda")
-a_cuda.copy_(a, non_blocking=True)
+
+# with torch.cuda.stream(s1):
+d = b * c
+
+with torch.cuda.stream(comm_stream):
+    a_cuda = torch.empty((4096, 4096), device="cuda")
+    a_cuda.copy_(a, non_blocking=True)
+
 torch.cuda.synchronize()
-print("syn", time.time() - start)
+print("asyn", time.time() - start)
+
+# torch.cuda.synchronize()
+# start = time.time()
+# e = b * c
+# a_cuda = torch.empty((4096, 4096), device="cuda")
+# a_cuda.copy_(a, non_blocking=True)
+# torch.cuda.synchronize()
+# print("syn", time.time() - start)
