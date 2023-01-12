8310c55e7bf5871ee9c62762134987b95673713e
superhao1995 <804673818@qq.com>
2023-01-12 14:21:26 +0800

    test

--- test_cuda_stream.py
+++ test_cuda_stream.py
@@ -1,44 +1,46 @@
 import time
 import torch
 
-# a = torch.rand((4096, 4096), pin_memory=True)
-# b = torch.rand((256, 256), device="cuda")
-# c = torch.rand((256, 256), device="cuda")
-#
-# comm_stream = torch.cuda.Stream()
-#
-# # asyn
-# torch.cuda.synchronize()
-# start = time.time()
-# d = b * c
-# with torch.cuda.stream(comm_stream):
-#     a_cuda = torch.empty((4096, 4096), device="cuda")
-#     a_cuda.copy_(a, non_blocking=True)
-# torch.cuda.synchronize()
-# print("asyn", time.time() - start)
-#
-# # syn
-# torch.cuda.synchronize()
-# start = time.time()
-# e = b * c
-# a_cuda = torch.empty((4096, 4096), device="cuda")
-# a_cuda.copy_(a, non_blocking=True)
-# torch.cuda.synchronize()
-# print("syn", time.time() - start)
+a = torch.rand((4096, 4096), pin_memory=True)
+b = torch.rand((16*1024, 16*1024), device="cuda")
+c = torch.rand((16*1024, 16*1024), device="cuda")
 
-a = torch.rand((16*1024, 16*1024), device="cuda:0")
-b = torch.rand((16*1024, 16*1024), device="cuda:0")
+comm_stream = torch.cuda.Stream()
+
+# asyn
 torch.cuda.synchronize()
 start = time.time()
-c = a * b
+d = b * c
+with torch.cuda.stream(comm_stream):
+    a_cuda = torch.empty((4096, 4096), device="cuda")
+    a_cuda.copy_(a, non_blocking=True)
 torch.cuda.synchronize()
-print(time.time() - start)
+print("asyn", time.time() - start)
 
-
-cpu_data = torch.rand((4096, 4096), pin_memory=True)
+# syn
 torch.cuda.synchronize()
 start = time.time()
-cuda_data = torch.empty((4096, 4096), device="cuda:0")
-cuda_data.copy_(cpu_data, non_blocking=True)
+e = b * c
+a_cuda = torch.empty((4096, 4096), device="cuda")
+a_cuda.copy_(a, non_blocking=True)
 torch.cuda.synchronize()
-print(time.time()-start)
+print("syn", time.time() - start)
+
+
+
+# a = torch.rand((16*1024, 16*1024), device="cuda:0")
+# b = torch.rand((16*1024, 16*1024), device="cuda:0")
+# torch.cuda.synchronize()
+# start = time.time()
+# c = a * b
+# torch.cuda.synchronize()
+# print(time.time() - start)
+#
+#
+# cpu_data = torch.rand((4096, 4096), pin_memory=True)
+# torch.cuda.synchronize()
+# start = time.time()
+# cuda_data = torch.empty((4096, 4096), device="cuda:0")
+# cuda_data.copy_(cpu_data, non_blocking=True)
+# torch.cuda.synchronize()
+# print(time.time()-start)
