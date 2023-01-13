00fdd025fbe32e2430a75fde271f5fa9eb6a7555
superhao1995 <804673818@qq.com>
2023-01-13 15:52:28 +0800

    test

--- mem_offload_optimize.py
+++ mem_offload_optimize.py
@@ -38,12 +38,12 @@ def memory_optimization(model: torch.nn.Module, inps: Dict[str, torch.Tensor], m
     offload_strategies_constructor = OffloadStrategiesConstructor(graph)
     offload_strategies_constructor.build_strategies_and_cost()
 
-    # solver = Solver(gm.graph, offload_strategies_constructor, memory_budget)
-    # solver._call_solver_greedy_v1()
-    # solver._call_solver_l2l()
+    solver = Solver(gm.graph, offload_strategies_constructor, memory_budget)
+    solver._call_solver_greedy_v1()
+    solver._call_solver_l2l()
 
-    solver = AsynGreedySolver(gm.graph, memory_budget)
-    solver._call_solver_greedy()
+    # solver = AsynGreedySolver(gm.graph, memory_budget)
+    # solver._call_solver_greedy()
 
     # print offload node
     print("****************** offload plan *******************")
