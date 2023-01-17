49a6e6881c873fa68c2b84b2803a6c965e726156
superhao1995 <804673818@qq.com>
2023-01-17 14:53:04 +0800

    test

--- solver.py
+++ solver.py
@@ -156,6 +156,7 @@ class AsynGreedySolver:
     def _call_solver_greedy(self):
         peak_mem_saving, total_mem_saving = self._compute_mem_saving()
         assert peak_mem_saving == 0 and total_mem_saving < 0
+        print("node num", len(self.nodes))
         print("init peak memory", self.peak_mem/1024**2, "MB")
         # record corresponding host node which prefetch the node to be offloaded
         node_to_node_map = {}
