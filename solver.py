72ad8b7a17ca4d2794b13d36605826602ef825c6
superhao1995 <804673818@qq.com>
2023-01-17 12:31:03 +0800

    test

--- solver.py
+++ solver.py
@@ -260,7 +260,6 @@ class AsynGreedySolver:
                 # tmp_profit = self._compute_offload_profit(tmp_peak_mem_saving, extra_comm_cost)
                 print("pre compute profit", tmp_total_mem_saving/1024**2, extra_comm_cost)
                 tmp_profit = self._compute_offload_profit(tmp_total_mem_saving, extra_comm_cost)
-                print(tmp_profit)
                 if self._compare_profit(tmp_profit, max_profit):
                     cancel_offload_node = node_to_offload
                     cancel_host_node = host_node
@@ -271,6 +270,11 @@ class AsynGreedySolver:
                 node_to_offload.node_info.offload_param_flag = True
                 host_node.node_info.node_to_prefetch = node_to_offload
 
+            print("aft each repair.......................")
+            for node_to_offload, host_node in self.node_to_node_map.items():
+                print(node_to_offload, host_node)
+            print("repair...peak_mem_saving", peak_mem_saving, self.peak_mem/1024**2)
+
             assert cancel_offload_node.node_info.syn_upload_flag == False
             cancel_offload_node.node_info.syn_upload_flag = True
             cancel_host_node.node_info.node_to_prefetch = None
@@ -278,9 +282,7 @@ class AsynGreedySolver:
             self.node_to_node_map[cancel_offload_node] = cancel_offload_node
             self.node_to_mem_saving_map[cancel_offload_node] += peak_mem_saving
 
-            print("repair...peak_mem_saving", peak_mem_saving)
-
-        print("aft repair.......................")
+        print("aft final repair.......................")
         for node_to_offload, host_node in self.node_to_node_map.items():
             print(node_to_offload, host_node)
 
