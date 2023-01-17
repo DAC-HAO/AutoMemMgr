1e35129062cdef25074af57d0e4cefcbb9a79b17
superhao1995 <804673818@qq.com>
2023-01-17 11:26:34 +0800

    unit test

--- tests/test_pin_memory.py
+++ tests/test_pin_memory.py
@@ -41,3 +41,25 @@ e_e.copy_(e)
 print(time.time() - start)
 del e
 del e_e
+
+print("copy cpu pin_memory to cuda time......................")
+f = torch.rand((20*1024, 20*1024), dtype=torch.half, pin_memory=True)
+torch.cuda.synchronize()
+start = time.time()
+f_cuda = torch.empty((20*1024, 20*1024), dtype=torch.half, device="cuda")
+f_cuda.copy_(f)
+torch.cuda.synchronize()
+print(time.time() - start)
+del f
+del f_cuda
+
+print("copy cuda to cpu pin_memory time......................")
+g = torch.rand((20*1024, 20*1024), dtype=torch.half, device="cuda")
+torch.cuda.synchronize()
+start = time.time()
+g_cpu = torch.empty((20*1024, 20*1024), dtype=torch.half, pin_memory=True)
+g_cpu.copy_(g)
+torch.cuda.synchronize()
+print(time.time() - start)
+del g
+del g_cpu
