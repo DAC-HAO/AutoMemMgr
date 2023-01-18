578af785fbffbf16fe9f7ea9fc39d464781d77f1
superhao1995 <804673818@qq.com>
2023-01-18 17:31:56 +0800

    test

--- model_utils.py
+++ model_utils.py
@@ -90,29 +90,29 @@ class NoLeafModule(nn.Module):
 
     def __init__(self, checkpoint=False) -> None:
         super().__init__()
-        self.proj1 = nn.Linear(1024, 2048)
-        # self.weight1 = nn.Parameter(torch.randn(2048, 2048))
+        self.proj1 = nn.Linear(512, 2048)
+        self.weight1 = nn.Parameter(torch.randn(2048, 2048))
         self.proj1_2 = nn.Linear(2048, 1024)
 
         self.proj2 = nn.Linear(1024, 2048)
-        # self.weight2 = nn.Parameter(torch.randn(2048, 2048))
-        self.proj2_2 = nn.Linear(2048, 1024)
+        self.weight2 = nn.Parameter(torch.randn(2048, 2048))
+        self.proj2_2 = nn.Linear(2048, 512)
 
-        self.proj3 = nn.Linear(1024, 2048)
-        # self.weight3 = nn.Parameter(torch.randn(2048, 2048))
-        self.proj3_2 = nn.Linear(2048, 1024)
+        self.proj3 = nn.Linear(512, 512)
+        self.weight3 = nn.Parameter(torch.randn(512, 512))
+        self.proj3_2 = nn.Linear(512, 1024)
 
     def forward(self, x):
         x = self.proj1(x)
-        # x = F.linear(x, self.weight1)
+        x = F.linear(x, self.weight1)
         x = self.proj1_2(x)
 
         x = self.proj2(x)
-        # x = F.linear(x, self.weight2)
+        x = F.linear(x, self.weight2)
         x = self.proj2_2(x)
 
         x = self.proj3(x)
-        # x = F.linear(x, self.weight3)
+        x = F.linear(x, self.weight3)
         x = self.proj3_2(x)
 
         return x
